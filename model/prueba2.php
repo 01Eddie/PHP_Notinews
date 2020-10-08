7ed0b80c043e762b142335aeb5778f0e4277271c
Eddie Hg <47726314+01Eddie@users.noreply.github.com>
2020-10-08 14:10:30 -0500

    Update prueba2.php

--- model/prueba2.php
+++ model/prueba2.php
@@ -18,7 +18,7 @@ try {
     $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
     $mail->Username   = 'eddiejesus1197@gmail.com';                     // SMTP username
-    $mail->Password   = 'ricardina';                               // SMTP password
+    $mail->Password   = '54asd4asd4as5da1d5s4ada54d5asd4a5sda51ds41a15d4a1sd54adas5d';                               // SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
